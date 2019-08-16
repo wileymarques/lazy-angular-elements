553a12e07dfdfcc82757b0d0c2adf0e88b267135
Wiley Marques <wileymarques@live.com>
2019-08-15 23:55:30 -0300

    Comandos para rodar projetos

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -3,10 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <le-root></le-root>
+    <button (click)="downloadLazyModule()">Download lazy module</button>
+    <div *ngIf="showLazyLoadedElement === true">
+      <lazy-loaded></lazy-loaded>
+    </div>
   `,
   styles: []
 })
 export class AppComponent {
+
+  public showLazyLoadedElement = false;
+
+  public downloadLazyModule(): void {
+    this.createScriptDownloadElement();
+  }
+
+  private createScriptDownloadElement(): void {
+    const lazyLoadedScriptTag = document.createElement('script');
+    lazyLoadedScriptTag.src = 'http://localhost:4201/main.js';
+    lazyLoadedScriptTag.onload = () => this.enableShowLazyLoadedElement();
+    document.head.appendChild(lazyLoadedScriptTag);
+  }
+
+  public enableShowLazyLoadedElement(): void {
+    // const lazyLoadedElementEl = document.createElement('lazy-loaded');
+    // const wrapperEl = document.getElementById('wrapper');
+    // wrapperEl.appendChild(lazyLoadedElementEl);
+    this.showLazyLoadedElement = true;
+  }
 
 }
